91dfa0220fdea79aaec8ae3360fd6935ae234453
alberto <albertmihai.moisan@gmail.com>
2025-10-22 15:55:46 +0300

    implemented character counter

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from stats import get_book_text
 from stats import word_counter
+from stats import char_counter
 
 def main():
     filepath = 'books/frankenstein.txt'
     #print(get_book_text(filepath))
     print(f'Found {word_counter(filepath)} total words')
+    print(char_counter(filepath))
 
 
 main()
--- stats.py
+++ stats.py
@@ -4,15 +4,30 @@ def get_book_text(filepath):
 
 def word_counter(filepath):
     text = get_book_text(filepath)
-    split = text.split()
-    words = 0
+    words = text.split()
+    return len(words)
 
-    ## Testing fuckery to try and eliminate non-alphabetical characters
+        ## Testing fuckery to try and eliminate non-alphabetical characters
     #alphabet = ['A','a','B','b','C','c','D','d','E','e','F','f','G',
     #'g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p',
     #'Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z']
     #is_word == True
 
-    for word in split:
-        words += 1
-    return words
+def char_counter(filepath):
+    text = get_book_text(filepath)
+    ltext = text.lower()
+    unique_ch = []
+    char_count = {}
+
+    for char in ltext:
+        if char not  in unique_ch:
+            unique_ch.append(char)
+
+    for ch in unique_ch:
+        counter = 0
+        for char in ltext:
+            if ch == char:
+                counter += 1
+                char_count[ch] = counter
+
+    return char_count
