a2ec8acc328b7fea662222d1ad38e4f6e2f40138
alberto <albertmihai.moisan@gmail.com>
2025-10-28 15:19:20 +0200

    Added support for user-entered book filepath

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import get_book_text, word_counter, char_counter, itemize_dict
+import sys
 
 def main():
+
+    if len(sys.argv) < 2:
+        print("Please introduce 2 arguments. Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     #print(get_book_text(filepath))
     #print(char_counter(filepath))
 
     print("============ BOOKBOT ============")
-    filepath = 'books/frankenstein.txt'
+    filepath = sys.argv[1]
 
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
@@ -19,7 +25,7 @@ def main():
     item_list.sort(key=sort_on, reverse=True)
 
     for item in item_list:
-        print(f"{item["char"]}:{item["num"]}")
+        print(f"{item["char"]}: {item["num"]}")
 
     print("============= END ===============")
 
